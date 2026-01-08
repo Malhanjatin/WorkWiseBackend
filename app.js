a56756da35e2e8aaf1bd2eb4a7d451d9151b0da1
Jatin Malhan <jatinmalhan2300@.in>
2026-01-08 20:01:49 +0530

    Update CORS for production frontend

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://workwiselybyme.netlify.app/",
     credentials: true,
   })
 );
